e921626964db26608fad8c8e717229fb3111d3ad
Daniele Viola <73640269+Kabewh@users.noreply.github.com>
2022-03-27 21:10:52 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar/Navbar'
+import Background from './components/Background'
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <Navbar/>
+      <Background />
+    </div>
+  );
+}
+
+export default App;
